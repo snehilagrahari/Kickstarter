4f03d7378a4298795f8deb751419ede454b23014
snehilagrahari <sag21062001@outlook.com>
2022-10-02 12:19:04 +0530

    final commit

--- Scripts/login.js
+++ Scripts/login.js
@@ -56,3 +56,26 @@ function login(event)
 }
 let aname=localStorage.getItem("account")||"Login";
 document.getElementById("login").innerText=aname;
+document.getElementById("login").addEventListener("click",displaylogout);
+let clickcount=0;
+function displaylogout()
+{
+    if(localStorage.getItem("account")!=undefined&clickcount%2==0)
+    {
+        clickcount++;
+        document.getElementById("logoutbox").style.display="block";
+    }
+    else if(localStorage.getItem("account")!=undefined&clickcount%2!=0)
+    {
+        clickcount++;
+        document.getElementById("logoutbox").style.display="none";
+    }
+    else
+        window.location.assign("login.html");
+}
+document.getElementById("logout").addEventListener("click",logout)
+function logout()
+{
+    localStorage.removeItem("account");
+    window.location.reload();
+}
--- Scripts/signup.js
+++ Scripts/signup.js
@@ -26,16 +26,27 @@ form.addEventListener("submit",signup)
 function signup(event)
 {
     event.preventDefault();
+    document.getElementById("existingemail").style.display="none";
     let details={
         name:document.getElementById("name").value,
         email:document.getElementById("email").value,
         pass:document.getElementById("pass").value
     };
     console.log(details);
+    let flag=false;
+    for(let i=0;i<userArr.length;i++)
+    {
+        if (details.email==userArr[i].email)
+            flag=true;
+    }
     if(details.name==""||details.email==""||details.pass=="")
     {
         p.style.display="block";
         return ;
+    }
+    else if(flag==true)
+    {
+        document.getElementById("existingemail").style.display="block";
     }
     else
     {
@@ -47,4 +58,27 @@ function signup(event)
     }
 }
 let aname=localStorage.getItem("account")||"Login";
-document.getElementById("acc").innerHTML=aname;
+document.getElementById("login").innerHTML=aname;
+document.getElementById("login").addEventListener("click",displaylogout);
+let clickcount=0;
+function displaylogout()
+{
+    if(localStorage.getItem("account")!=undefined&clickcount%2==0)
+    {
+        clickcount++;
+        document.getElementById("logoutbox").style.display="block";
+    }
+    else if(localStorage.getItem("account")!=undefined&clickcount%2!=0)
+    {
+        clickcount++;
+        document.getElementById("logoutbox").style.display="none";
+    }
+    else
+        window.location.assign("login.html");
+}
+document.getElementById("logout").addEventListener("click",logout)
+function logout()
+{
+    localStorage.removeItem("account");
+    window.location.reload();
+}
